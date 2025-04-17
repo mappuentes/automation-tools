0d3b49e34448062c5e14f82eca3a3ebd74621c58
mappuentes <ma.puentes@alumnos.upm.es>
2025-04-17 17:56:09 +0000

    v.4 ---una vm en cada server

--- main.tf
+++ main.tf
@@ -8,41 +8,74 @@ terraform {
 }
 
 provider "libvirt" {
-  uri = "qemu:///system"
+  alias = "gaspar"
+  uri   = "qemu:///system"
+}
+provider "libvirt" {
+  alias = "melchor"
+  uri   = "qemu+ssh://administrador@10.0.0.11/system?keyfile=~/.ssh/id_rsa_melchor"
 }
 
-variable "vm_name" {
+variable "vm_master" {
   description = "The name of the virtual machine"
-  default     = "ubuntu"
+  default     = "master"
+  type        = string
+}
+variable "vm_worker" {
+  description = "The name of the virtual machine"
+  default     = "worker"
   type        = string
 }
 
-resource "libvirt_volume" "ubuntu-qcow2" {
-  name   = "ubuntu20.04"
-  pool   = "default"
-  source = "${path.module}/packer/output-focal/ubuntu-focal.img"
-  format = "qcow2"
+resource "libvirt_volume" "master-qcow2" {
+  provider = libvirt.gaspar
+  name     = "masterUbuntu20.04"
+  pool     = "default"
+  source   = "${path.module}/packer/output-focal/ubuntu-focal.img"
+  format   = "qcow2"
 }
 
-data "template_file" "user_data" {
+resource "libvirt_volume" "worker1-qcow2" {
+  provider = libvirt.melchor
+  name     = "worker1Ubuntu20.04"
+  pool     = "default"
+  source   = "${path.module}/packer/output-focal/ubuntu-focal.img"
+  format   = "qcow2"
+}
+
+data "template_file" "user_data_master" {
   template = file("userdata.tpl")
   vars = {
-    HOSTNAME = var.vm_name
+    HOSTNAME = var.vm_master
   }
 }
 
-resource "libvirt_cloudinit_disk" "cloudinit" {
-  name      = "${var.vm_name}-cloudinit.iso"
-  user_data = data.template_file.user_data.rendered
+resource "libvirt_cloudinit_disk" "cloudinit_master" {
+  provider  = libvirt.gaspar
+  name      = "${var.vm_master}-cloudinit.iso"
+  user_data = data.template_file.user_data_master.rendered
 }
 
-resource "libvirt_domain" "vm" {
-  name   = var.vm_name
+data "template_file" "user_data_worker" {
+  template = file("userdata.tpl")
+  vars = {
+    HOSTNAME = var.vm_worker
+  }
+}
+
+resource "libvirt_cloudinit_disk" "cloudinit_worker" {
+  provider  = libvirt.melchor
+  name      = "${var.vm_worker}-cloudinit.iso"
+  user_data = data.template_file.user_data_worker.rendered
+}
+
+resource "libvirt_domain" "master" {
+  name   = var.vm_master
   memory = 2048
   vcpu   = 2
 
   disk {
-    volume_id = libvirt_volume.ubuntu-qcow2.id
+    volume_id = libvirt_volume.master-qcow2.id
   }
 
   network_interface {
@@ -50,7 +83,35 @@ resource "libvirt_domain" "vm" {
     wait_for_lease = true
   }
 
-  cloudinit = libvirt_cloudinit_disk.cloudinit.id
+  cloudinit = libvirt_cloudinit_disk.cloudinit_master.id
+
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  graphics {
+    type        = "vnc"
+    listen_type = "address"
+  }
+}
+resource "libvirt_domain" "worker" {
+  provider = libvirt.melchor
+  name     = var.vm_worker
+  memory   = 2048
+  vcpu     = 2
+
+  disk {
+    volume_id = libvirt_volume.worker1-qcow2.id
+  }
+
+  network_interface {
+    network_name   = "default"
+    wait_for_lease = true
+  }
+
+  cloudinit = libvirt_cloudinit_disk.cloudinit_worker.id
 
   console {
     type        = "pty"
